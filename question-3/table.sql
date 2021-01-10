65d289b2808100848ca552f1b8b1a1787bd8aeb5
Gaurav Kumar <gauravsyr10@gmail.com>
2021-01-10 18:37:02 +0530

    added some line

--- question-3/table.sql
+++ question-3/table.sql
@@ -74,31 +74,26 @@ group by s_id;
 
 
 declare
-	n number;
-	i number;
-	flag number;
- 
+n number;
+i number;
+temp number;
+
 begin
-	i:=2;
-	flag:=1;
-	n:=&n;
- 
-	for i in 2..n/2
-	loop
-		if mod(n,i)=0
-		then
-			flag:=0;
-			exit;
-		end if;
-	end loop;
- 
-	if flag=1
-	then
-		dbms_output.put_line('prime');
-	else
-		dbms_output.put_line('not prime');
-	end if;
+n := 13;
+i := 2;
+temp := 1;
+
+for i in 2..n/2 loop
+if mod(n,i) = 0 then
+temp := 0;
+exit;
+end if;
+end loop;
+if temp = 1 then
+dbms_output.put_line('Prime number');
+else
+dbms_output.put_line('Not a prime number');
+end if;
 end;
-/
 
 
